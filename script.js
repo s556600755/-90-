77b029876de261fbd312a4255dfcc61950b91b5b
wenjie <s95175388@gmail.com>
2026-05-28 22:03:24 +0800

    完成 day3

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const name = 'Wenjie';
-const age =  28;
+const age = 28;
 
-console.log('我叫'+name+'今年'+age)
+console.log('我叫' + name + '今年' + age)
 
-const height = 170; 
-const weight = 50 ;
+const height = 170;
+const weight = 50;
 
-const bmi = weight/((height/100)*(height/100));
+const bmi = weight / ((height / 100) * (height / 100));
 
 console.log(bmi);
+
+const score = 59;
+score >= 60 ? console.log('及格分數為' + score) : console.log('不及格 需要補考')
+
+const admin = 'user';
+const password = 1234;
+
+(admin === 'user' && password === 1234) ? console.log('登入成功') : console.log('帳號或密碼錯誤')
+
+
+
