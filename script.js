e25f96b54f5b5540c45a2995ba3142129d3add5e
wenjie <s95175388@gmail.com>
2026-05-28 21:44:06 +0800

    完成 day2

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const name = 'Wenjie';
 const age =  28;
 
 console.log('我叫'+name+'今年'+age)
+
+const height = 170; 
+const weight = 50 ;
+
+const bmi = weight/((height/100)*(height/100));
+
+console.log(bmi);
