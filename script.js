56e2754957ee5cb214b7c599dffa9d61f1dbb2a1
wenjie <s95175388@gmail.com>
2026-05-29 22:53:20 +0800

    完成 day9 bmi計算器 名字歡迎系統

--- script.js
+++ script.js
@@ -1,88 +1,110 @@
-const name = 'Wenjie';
-const age = 28;
+// const name = 'Wenjie';
+// const age = 28;
 
-console.log('我叫' + name + '今年' + age)
+// console.log('我叫' + name + '今年' + age)
 
-const height = 170;
-const weight = 50;
+// const height = 170;
+// const weight = 50;
 
-const bmi = weight / ((height / 100) * (height / 100));
+// const bmi = weight / ((height / 100) * (height / 100));
 
-console.log(bmi);
+// console.log(bmi);
 
-const score = 59;
-score >= 60 ? console.log('及格分數為' + score) : console.log('不及格 需要補考')
+// const score = 59;
+// score >= 60 ? console.log('及格分數為' + score) : console.log('不及格 需要補考')
 
-const admin = 'user';
-const password = 1234;
+// const admin = 'user';
+// const password = 1234;
 
-(admin === 'user' && password === 1234) ? console.log('登入成功') : console.log('帳號或密碼錯誤')
+// (admin === 'user' && password === 1234) ? console.log('登入成功') : console.log('帳號或密碼錯誤')
 
-function calculatePrice(price, quantity) {
-    const totalPrice = price * quantity
-    return totalPrice;
-}
+// function calculatePrice(price, quantity) {
+//     const totalPrice = price * quantity
+//     return totalPrice;
+// }
 
-const result = calculatePrice(60, 10)
+// const result = calculatePrice(60, 10)
 
-console.log('總價:' + result + "元");
+// console.log('總價:' + result + "元");
 
-const foods = ["raman", "pizza"];
-foods.push('sushi')
-console.log(foods)
+// const foods = ["raman", "pizza"];
+// foods.push('sushi')
+// console.log(foods)
 
-const scores = [80, 90, 70];
-let total = 0;
-for (let i = 0; i < scores.length; i++) {
-    total += scores[i];
-}
-console.log(total)
+// const scores = [80, 90, 70];
+// let total = 0;
+// for (let i = 0; i < scores.length; i++) {
+//     total += scores[i];
+// }
+// console.log(total)
 
-const users = [
-    {
-        name: "Wen",
-        age: 28,
-        city: "Tainan",
-        hobby: '網球'
-    },
-    {
-        name: "Amy",
-        age: 24,
-        city: "Taipei",
-        hobby: '足球'
-    },
-    {
-        name: "Huang",
-        age: 30,
-        city: "Taipei",
-        hobby: '桌球'
-    }
-];
+// const users = [
+//     {
+//         name: "Wen",
+//         age: 28,
+//         city: "Tainan",
+//         hobby: '網球'
+//     },
+//     {
+//         name: "Amy",
+//         age: 24,
+//         city: "Taipei",
+//         hobby: '足球'
+//     },
+//     {
+//         name: "Huang",
+//         age: 30,
+//         city: "Taipei",
+//         hobby: '桌球'
+//     }
+// ];
 
-for (let i = 0; i < users.length; i++) {
-    console.log("名字：" + users[i].name);
-    console.log("年齡：" + users[i].age);
-    console.log("城市：" + users[i].city);
-    console.log("興趣：" + users[i].hobby)
-    console.log("------------");
-}
+// for (let i = 0; i < users.length; i++) {
+//     console.log("名字：" + users[i].name);
+//     console.log("年齡：" + users[i].age);
+//     console.log("城市：" + users[i].city);
+//     console.log("興趣：" + users[i].hobby)
+//     console.log("------------");
+// }
 
-const title = document.querySelector('#title');
-let content = ''
-for (let i = 0; i < users.length; i++) {
-    console.log(users[i])
-    content += `
-    姓名: ${users[i].name} <br> 
-    居住地: ${users[i].city} <br> 
-    年齡: ${users[i].age} <br> 
-    興趣: ${users[i].hobby} <br>
-    `
+// const title = document.querySelector('#title');
+// let content = ''
+// for (let i = 0; i < users.length; i++) {
+//     console.log(users[i])
+//     content += `
+//     姓名: ${users[i].name} <br> 
+//     居住地: ${users[i].city} <br> 
+//     年齡: ${users[i].age} <br> 
+//     興趣: ${users[i].hobby} <br>
+//     `
 
     
 
-}console.log(content)
+// }console.log(content)
 
-title.innerHTML = `<p>${content}</p>`
+// title.innerHTML = `<p>${content}</p>`
 
-const body = document.querySelector('#body');
+// const body = document.querySelector('#body');
 
+const input = document.querySelector('#input');
+const btn  = document.querySelector('#btn');
+const message = document.querySelector('#message');
+
+btn.addEventListener('click', () => {
+    const content = input.value;
+    console.log(input.value)
+    message.textContent = "歡迎你,"+content
+    input.value = '';
+});
+
+const bmiBtn = document.querySelector('#bmiBtn');
+const weightInput = document.querySelector('#weightInput');
+const heightInput = document.querySelector('#heightInput');
+const output = document.querySelector('.output');
+bmiBtn.addEventListener('click', () => {
+    const weightValue = weightInput.value;
+    const heightValue = heightInput.value;
+    const result = weightValue / ((heightValue/100)**2)
+    console.log(result)
+    output.textContent = result.toFixed(2);
+});
